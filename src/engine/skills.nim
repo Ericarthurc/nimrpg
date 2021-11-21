1a3a8eac600831ff57852fb72ffa1fedc775aa9a
Eric Christensen <hebrew.8.12@gmail.com>
2021-11-21 09:18:36 -0800

    11/21/21 09:18

--- src/engine/skills.nim
+++ src/engine/skills.nim
@@ -1,4 +1,4 @@
-import tables
+import tables, std/strformat
 
 type
   Skill = object
@@ -9,8 +9,8 @@ type
     manaRequirement: int
     levelRequirement: int
 
-# proc `$`(s: Skill): string =
-#   result = fmt"{s.name}"
+proc `$`(s: Skill): string =
+  result = fmt"{s.skillName}"
 
 var skillTable = initTable[string, seq[Skill]]()
 skillTable["warrior"] = @[]
@@ -31,25 +31,22 @@ skillTable["warrior"].add(Skill(
     manaRequirement: 10
   ))
 
+skillTable["wizard"] = @[]
+skillTable["wizard"].add(Skill(
+    id: 1,
+    skillName: "Lightning Strike",
+    description: "Lightning strikes from above!",
+    damage: 12,
+    levelRequirement: 0,
+    manaRequirement: 10,
+))
+
+skillTable["ranger"] = @[]
+
+skillTable["thief"] = @[]
+
+skillTable["guardian"] = @[]
+
+skillTable["necromancer"] = @[]
+
 echo skillTable
-
-# var warriorSkills = {"warrior": @[
-# Skill(
-#     id: 1,
-#     skillName: "head butt",
-#     description: "Swing your head forward with mighty force!",
-#     damage: 100,
-#     levelRequirement: 0,
-#     manaRequirement: 5
-# ),
-# Skill(
-#     id: 2,
-#     skillName: "slash",
-#     description: "Slash your weapon forward striking whats in front of you!",
-#     damage: 80,
-#     levelRequirement: 1,
-#     manaRequirement: 10
-# ),
-# ]}.toTable
-
-# echo warriorSkills
